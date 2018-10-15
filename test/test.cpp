a918428ac0215d9afad04b4b87fe0ae0a041b125
mayavan <mayavan95@gmail.com>
2018-10-15 00:36:00 -0400

    Included copyright in every files

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,28 @@
 /**
  * @file test.cpp
+ * @brief RRT Planner - testing
  * @author RajendraMayavan
  * @copyright MIT License
- *
- * @brief RRT Planner - testing
+
+ * Copyright (c) 2018 Mayavan
+
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  */
 
@@ -30,8 +49,7 @@ TEST(getState, should_pass) {
   EXPECT_LT(150, ob.getState(25, 25));
 }
 
-TEST(checkObstacle, should_pass)
-{
+TEST(checkObstacle, should_pass) {
   std::pair<int, int> firstPoint(205, 1);
   MapManager ob(fileLocation);
   EXPECT_TRUE(!ob.checkObstacle(firstPoint));
